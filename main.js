b67b782ebc878c5d6a820f742c2781091634f5e2
26823 <26823@greijdanus.nl>
2026-09-20 08:59:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ window.addEventListener('load', function(){
                     this.frameX++;
 
             // Aanval klaar
-                    if (this.frameX >= 6) {
+                    if (this.frameX >= 5) {
                         this.frameX = this.minFrame;
                         this.frameY = this.minFrame;
                         this.attacking = false;
